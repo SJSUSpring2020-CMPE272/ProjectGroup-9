84045df8bf2eec9e8e40de1cb0f5914b61709a00
anylee2142@gmail.com <Anylee2142@gmail.com>
2020-05-01 17:17:39 -0700

    Changes 1. Modift logics 2. Add a test 3. Add a sample upload image

--- backEnd/index.js
+++ backEnd/index.js
@@ -7,6 +7,18 @@ var mime = require('mime');
 var multer = require('multer');
 // var upload = multer({dest: 'uploads/'});
 
+// const {Storage} = require('@google-cloud/storage');
+var bodyParser = require('body-parser');
+// var config = {
+//     projectId: 'my-expense-keeper',
+//     keyFilename: './keyFile.json'
+//   };
+ 
+//   let vision = require('@google-cloud/vision')({
+//     projectId: 'my-expense-keeper',
+//     keyFilename: './keyFile.json'
+//   });
+
 
   // Creates a client
 var app = express();
@@ -39,6 +51,10 @@ app.get('./uploadImage', function(req, res, next) {
     
 })
 
+// Get the uploaded image
+// Image is uploaded to req.file.path
+// Turn image into Base64 so we can display it easily
+
 function base64Image(src) {
   var data = fs.readFileSync(src).toString('base64');
   return util.format('data:%s;base64,%s', mime.lookup(src), data);
@@ -48,3 +64,5 @@ app.post('/upload_file', upload.any(), (req, res) => {
 });
 app.listen(3001);
 console.log("Server Listening on port 3001");
+
+// 325c24f9017a3f8559d862d2a75105c0
